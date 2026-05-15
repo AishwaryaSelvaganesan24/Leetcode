b1f8cf736436edf7d4fd8372171e2e9434908289
Aishwarya S <ishwaryaselvaganesan@gmail.com>
2026-05-15 16:07:28 +0530

    Time: 2 ms (60.46%) | Memory: 43.9 MB (49.26%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -2,32 +2,31 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
        int n = code.length;
        int[]arr = new int[n];
+       for(int i=0;i<n;i++){
+        int sum =0;
        if(k==0){
-            for(int i=0;i<n;i++){
-               arr[i] = 0;
-            }
+            return arr;
        }
        else if (k>0){
-          for(int i=0;i<n;i++){
-            int sum =0;
+          
             for(int j=1;j<=k;j++){
                 int ind = (i+j)%n;
                 sum+=code[ind];
             }
             arr[i] = sum;
-          }
+          
        }  
        else if (k<0){
           
-          for(int i=0;i<n;i++){
-            int sum =0;
+          
             for(int j=1;j<=Math.abs(k);j++){
                 int ind = (i-j+n)%n;
                 sum+=code[ind];
             }
             arr[i]= sum;
-          }
-       }    
+          
+       }  
+       }  
 
        return arr;
     }
